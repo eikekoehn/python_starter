00095917e7c35cde2dd63e48092ed2df59608448
Simona Meiler <simonameiler@simona-meilers-macbook.local>
2019-02-05 14:12:14 +0100

    test

--- python_starter.py
+++ python_starter.py
@@ -1,3 +1,4 @@
+SIMONAS UPDATES
 This file is a Python script, going through the most important bascis for using Python in the world of oceanographic research.
 It is meant to help students and scientists unfamiliar with Python as a Progamming Language. Rather than a complete and official introduction, it is kept rather informal and leaves out lots of details. It is just intended to get the target group started.
 author: Eike Köhn
